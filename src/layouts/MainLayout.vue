a7e6c2280e71f3eaeaa3584fe18c883c44018bc0
ardha78 <ardhakrs@gmail.com>
2023-01-25 00:44:14 +0800

    add api & template

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+  <navbar/>
+  <sidebar/>  
+
+    <div class="content-wrapper">
+      <router-view/>
+    </div>
+
+    <footer class="main-footer">
+      <strong
+        >Copyright &copy; 
+        <a href="https://balisolutionbiz.com/">Bali Solution Biz</a>.</strong
+      >
+      All rights reserved.
+    </footer>
+
+    <aside class="control-sidebar control-sidebar-dark"></aside>
+  </div>
+</template>
+<script>
+import Navbar from "./inc/Navbar.vue"
+import Sidebar from "./inc/Sidebar.vue"
+
+export default {
+    components: {
+        Navbar, Sidebar,
+    }
+}
+</script>
